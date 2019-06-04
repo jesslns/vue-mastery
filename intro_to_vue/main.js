c36097ccd6196c235b27754df5e1eeed8b5387b7
nsleung <nsleung@esrichina.hk>
2019-06-04 14:34:55 +0800

    Add computed properties

--- intro_to_vue/main.js
+++ intro_to_vue/main.js
@@ -1,9 +1,11 @@
 var app = new Vue({
     el: '#app',
     data: {
+        brand: 'Vue Mastery',
         product: 'Socks',
-        image: "./assets/vmSocks-green-onWhite.jpg",
-        inStock: true,
+        // image: "./assets/vmSocks-green-onWhite.jpg",
+        // to update more than just image:
+        selectedVariant: 0,
         // inventory: 100
         details: ["80% cotton", "20% polyester", "size 23-25"],
 
@@ -11,12 +13,14 @@ var app = new Vue({
             {
                 variantID: 2234,
                 variantColour: "green",
-                variantImage: "./assets/vmSocks-green-onWhite.jpg"
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 0
             },
             {
                 variantID: 2235,
                 variantColour: "blue",
-                variantImage: "./assets/vmSocks-blue-onWhite.jpg"
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 8
             }
         ],
 
@@ -27,8 +31,20 @@ var app = new Vue({
         addToCart: function () { 
             this.cart +=1  // refers to cart in the data
         },
-        updateProduct(variantImage) {  // ES6 format
-            this.image = variantImage
+        updateProduct(index) {  // ES6 format
+            this.selectedVariant = index
+            console.log(index)
+        }
+    },
+    computed: {
+        title() {
+          return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity
         }
     }
 })
